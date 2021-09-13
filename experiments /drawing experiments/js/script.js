c33c7f72c1a89c78dd94a686119f98c5126ded4e
SaraGraveline <graveline.sara@gmail.com>
2021-09-13 13:50:31 -0400

    Drawing experiment: Drawing a flower

--- experiments /drawing experiments/js/script.js
+++ experiments /drawing experiments/js/script.js
@@ -21,7 +21,43 @@ function preload() {
 Description of setup
 */
 function setup() {
+  createCanvas (500, 500);
+
   background(255, 270, 98);
+
+  ellipseMode(CORNER);
+  ellipse(250, 250, 100, 100);
+  ellipse(250, 250, 80, 80);
+  ellipse(250, 250, 60, 60);
+  ellipse(250, 250, 40, 40);
+  ellipse(250, 250, 20, 20);
+  ellipse(250, 250, 10, 10);
+
+  ellipse(150, 150, 100, 100);
+  ellipse(170, 170, 80, 80);
+  ellipse(190, 190, 60, 60);
+  ellipse(210, 210, 40, 40);
+  ellipse(230, 230, 20, 20);
+  ellipse(240, 240, 10, 10);
+
+  ellipse(250, 150, 100, 100);
+  ellipse(250, 170, 80, 80);
+  ellipse(250, 190, 60, 60);
+  ellipse(250, 210, 40, 40);
+  ellipse(250, 230, 20, 20);
+  ellipse(250, 240, 10, 10);
+
+  ellipse(150, 250, 100, 100);
+  ellipse(170, 250, 80, 80);
+  ellipse(190, 250, 60, 60);
+  ellipse(210, 250, 40, 40);
+  ellipse(230, 250, 20, 20);
+  ellipse(240, 250, 10, 10);
+
+  ellipseMode(CENTER);
+  ellipse(250, 250, 6, 6);
+
+
 }
 
 
