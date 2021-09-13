88b507501036264ceb7aef65806d797a63c8f264
SaraGraveline <graveline.sara@gmail.com>
2021-09-13 14:28:04 -0400

    Drawing experiment: Added color to my flower

--- experiments /drawing experiments/js/script.js
+++ experiments /drawing experiments/js/script.js
@@ -23,40 +23,65 @@ Description of setup
 function setup() {
   createCanvas (500, 500);
 
-  background(255, 270, 98);
+  background(0, 0, 0);
 
   ellipseMode(CORNER);
+
+  noStroke();
+  fill(161, 161, 2);
   ellipse(250, 250, 100, 100);
+  fill(184, 184, 20);
   ellipse(250, 250, 80, 80);
+  fill(230, 230, 23);
   ellipse(250, 250, 60, 60);
+  fill(245, 245, 78);
   ellipse(250, 250, 40, 40);
+  fill(252, 252, 144);
   ellipse(250, 250, 20, 20);
+  fill(247, 247, 193);
   ellipse(250, 250, 10, 10);
 
+  noStroke();
+  fill(7, 92, 163);
   ellipse(150, 150, 100, 100);
+  fill(15, 125, 217);
   ellipse(170, 170, 80, 80);
+  fill(35, 151, 247);
   ellipse(190, 190, 60, 60);
+  fill(88, 174, 245);
   ellipse(210, 210, 40, 40);
+  fill(127, 192, 245);
   ellipse(230, 230, 20, 20);
+  fill(173, 214, 247);
   ellipse(240, 240, 10, 10);
 
+  noStroke();
+  fill(6, 148, 79);
   ellipse(250, 150, 100, 100);
+  fill(11, 181, 98);
   ellipse(250, 170, 80, 80);
+  fill(22, 245, 136);
   ellipse(250, 190, 60, 60);
+  fill(100, 250, 180);
   ellipse(250, 210, 40, 40);
+  fill(160, 250, 210);
   ellipse(250, 230, 20, 20);
+  fill(204, 255, 230);
   ellipse(250, 240, 10, 10);
 
+  noStroke();
+  fill(173, 7, 7);
   ellipse(150, 250, 100, 100);
+  fill(245, 22, 22);
   ellipse(170, 250, 80, 80);
+  fill(250, 77, 77);
   ellipse(190, 250, 60, 60);
+  fill(247, 119, 119);
   ellipse(210, 250, 40, 40);
+  fill(245, 159, 159);
   ellipse(230, 250, 20, 20);
+  fill(245, 201, 201);
   ellipse(240, 250, 10, 10);
-
-  ellipseMode(CENTER);
-  ellipse(250, 250, 6, 6);
-
 
 }
 
