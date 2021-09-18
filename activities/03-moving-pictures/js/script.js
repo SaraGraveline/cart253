419d780ea5de17600a64f17bed1269439c72a1e9
SaraGraveline <graveline.sara@gmail.com>
2021-09-18 19:45:58 -0400

    A3: completed the moving picture activity

--- activities/03-moving-pictures/js/script.js
+++ activities/03-moving-pictures/js/script.js
@@ -1,33 +1,71 @@
 /**
-Title of Project
-Author Name
+A3 - Moving Pictures
+Sara Graveline
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+This project shows two circles moving towards each other.
 */
 
-"use strict";
+//background variables
+let bg = {
+  r: 0,
+  g: 0,
+  b: 0,
+};
 
+// Circle 1 variables
+let circle1 = {
+  x: 0,
+  y: 250,
+  size: 100,
+  growthRate: 1,
+  speed: 1,
+  fill: 255,
+  alpha: 200,
+};
 
-/**
-Description of preload
-*/
-function preload() {
+// Circle 2 variables
+let circle2 = {
+  x: 500,
+  y: 250,
+  size: 75,
+  sizeRatio: 0.5,
+  speed: -1,
+  fill: 255,
+  alpha: 200,
+};
 
-}
-
-
-/**
-Description of setup
-*/
+//setup()
+//
+//creates canvas and no stroke
 function setup() {
-
+  createCanvas(500, 500);
+  noStroke();
 }
 
 
-/**
-Description of draw()
-*/
+//draw()
+//
+//draws two moving circles
 function draw() {
+
+  //background
+  background(bg.r, bg.b, bg.g);
+  bg.r = map(circle1.size, 100, width, 0, 255);
+
+  //left circle
+  circle1.x += circle1.speed;
+  circle1.x = constrain(circle1.x, 0, width / 2);
+  circle1.size += circle1.growthRate;
+  circle1.size = constrain(circle1.size, 0, width);
+  fill(circle1.fill, circle1.alpha);
+  ellipse(circle1.x, circle1.y, circle1.size);
+
+  //Right circle
+  circle2.x += circle2.speed;
+  circle2.x = constrain(circle2.x, width/2, width);
+  circle2.size = circle1.size * circle2.sizeRatio;
+  fill(circle2.fill, circle2.alpha);
+  ellipse(circle2.x, circle2.y, circle2.size);
+
 
 }
