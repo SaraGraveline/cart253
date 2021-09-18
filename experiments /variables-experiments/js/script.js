9e59fba3eb1e3d122137d85681885e5a71966770
SaraGraveline <graveline.sara@gmail.com>
2021-09-18 18:20:30 -0400

    Learned about changing variables

--- experiments /variables-experiments/js/script.js
+++ experiments /variables-experiments/js/script.js
@@ -11,6 +11,8 @@ let backgroundShade = 0;
 let circleX = 250;
 let circleY = 250;
 let circleSize = 200;
+let circleSpeed = 2;
+let circleAcceleration = 0.25;
 
 
 //Setup()
@@ -25,6 +27,9 @@ function setup() {
 //
 //shows different colors, and perfectly centered rectangle.
 function draw() {
+  backgroundShade = backgroundShade + 1;
   background(backgroundShade);
+  circleX += circleSpeed
+  circleX += circleAcceleration;
   ellipse(circleX, circleY, circleSize);
 }
