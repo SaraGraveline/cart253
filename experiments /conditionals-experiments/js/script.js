4b6b9cbc2efff7f29ce6c43a18edb02028470ce6
SaraGraveline <graveline.sara@gmail.com>
2021-09-27 23:22:58 -0400

    conditionals-experiments: movement

--- experiments /conditionals-experiments/js/script.js
+++ experiments /conditionals-experiments/js/script.js
@@ -8,16 +8,17 @@ This document is about experimenting with conditionals.
 "use strict";
 
 //creating variables
-let bg = {
-  r: 0,
-  g: 0,
-  b: 0,
-}
 
 let circle = {
   x: 250,
   y: 250,
-  size: 100
+  size: 100,
+  vx: 0,
+  vy: 0,
+  ax: 0,
+  ay: 0,
+  acceleration: 0.1,
+  maxSpeed: 10
 };
 
 
@@ -30,13 +31,30 @@ function setup() {
 //Draw()
 //
 function draw() {
-  background(bg.r, bg.g, bg.b);
+  background(0);
+
+  if (mouseX < circle.x) {
+    circle.ax = -circle.acceleration;
+  }
+  else {
+    circle.ax = circle.acceleration;
+  }
+
+  if (mouseY < circle.y) {
+    circle.ay = -circle.acceleration;
+  }
+  else {
+    circle.ay = circle.acceleration;
+  }
+
+  circle.vx += circle.ax;
+  circle.vx = constrain (circle.vx, -circle.maxSpeed, circle.maxSpeed);
+  circle.vy += circle.ay;
+circle.vy = constrain (circle.vy, -circle.maxSpeed, circle.maxSpeed);
+
+  circle.x += circle.vx;
+  circle.y += circle.vy;
 
   ellipse(circle.x, circle.y, circle.size);
-}
 
-  function mouseWheel() {
-    bg.r = random(0, 255);
-    bg.g = random(0, 255);
-    bg.b = random(0, 255);
-  }
+}
