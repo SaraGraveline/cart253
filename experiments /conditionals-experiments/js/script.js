5759a63a28dc30a36d6e7123b7398d8c3bea951c
SaraGraveline <graveline.sara@gmail.com>
2021-09-27 23:11:39 -0400

    conditionals-experiments: Mouse input

--- experiments /conditionals-experiments/js/script.js
+++ experiments /conditionals-experiments/js/script.js
@@ -8,48 +8,35 @@ This document is about experimenting with conditionals.
 "use strict";
 
 //creating variables
+let bg = {
+  r: 0,
+  g: 0,
+  b: 0,
+}
+
 let circle = {
-  x: undefined,
-  y: undefined,
+  x: 250,
+  y: 250,
   size: 100
 };
 
-  let dangerZone = {
-    x: 250,
-    y: 250,
-    size: 200
-  };
 
 //Setup()
 //
 function setup() {
   createCanvas (500, 500);
-
-  circle.x = random(0, width);
-  circle.y = random(0, height);
-
-  let d = dist(circle.x,circle.y, dangerZone.x, dangerZone.y);
-  while (d <circle.size/2 + dangerZone.size/2) {
-    circle.x = random(0, width);
-    circle.y = random(0, height);
-    d = dist(circle.x, circle.y, dangerZone.x, dangerZone.y);
-  }
 }
-
 
 //Draw()
 //
 function draw() {
-  background(0);
+  background(bg.r, bg.g, bg.b);
 
-  //danger zone
-  noFill();
-  stroke(255, 0, 0);
-  ellipse(dangerZone.x, dangerZone.y, dangerZone.size);
-
-  //random circle
-  fill(150);
-  noStroke();
   ellipse(circle.x, circle.y, circle.size);
-
 }
+
+  function mouseWheel() {
+    bg.r = random(0, 255);
+    bg.g = random(0, 255);
+    bg.b = random(0, 255);
+  }
