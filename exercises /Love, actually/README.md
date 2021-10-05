51566759d7e427d66d413153d1f56aa8fdbd2553
SaraGraveline <graveline.sara@gmail.com>
2021-10-05 17:50:29 -0400

    Exercise: Love, actually - finished.

--- exercises /Love, actually/README.md
+++ exercises /Love, actually/README.md
@@ -1,3 +1,12 @@
-# Template p5 project
+#Exercise: Love, actually
+Sara Graveline
 
-This is the README file for the entire project. For more official projects you should write information here about the nature of the project, your name, any special explanations of how the project works, etc.
+Goal: Don't get caught by negativity.
+This project is mostly based on functions and how to use them. The main goal of this game is to, `not get caught be the negativity,` which is around us all the time.
+The user have to try to get their way to the positivity by using the arrow keys and while avoiding all the negativity on their path.
+The negativity are the bounce pink balls.
+The positivity is the purple ball which moves very randomly and is chaotic in a way.
+The user-control circle is the yellow circle which moves with the arrow keys on the keyword.
+To start the game the user has to press on the mouse to move to the simulation state.
+If the user-control circle touches the pink circles, the game ends and the following message, `Oops, you failed :(.` And the user has to reload the page to start again.
+However, if the user successfully find its way and touches the purple circle, the game ends with this message, `Hooray!!! you won`.
--- exercises /Love, actually/js/script.js
+++ exercises /Love, actually/js/script.js
@@ -1,9 +1,16 @@
 /**
-Title of Project
-Author Name
+Exercise: Love, actually
+Sara Graveline
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Goal: Don't get caught by negativity.
+This project is mostly based on functions and how to use them. The main goal of this game is to `not get caught be the negativity` which is around us all the time.
+The user have to try to get their way to the positivity by using the arrow keys and while avoiding all the negativity on their path.
+The negativity are the bounce pink balls.
+The positivity is the purple ball which moves very randomly and is chaotic in a way.
+The user-control circle is the yellow circle which moves with the arrow keys on the keyword.
+To start the game the user has to press on the mouse to move to the simulation state.
+If the user-control circle touches the pink circles, the game ends and the following message, `Oops, you failed :(.` And the user has to reload the page to start again.
+However, if the user successfully find its way and touches the purple circle, the game ends with this message, `Hooray!!! you won`.
 */
 
 "use strict";
@@ -35,9 +42,7 @@ let nonUser = {
    size: 50,
  };
 
-let titleString = `Don't get caught by negativity!`;
-let endingString1 = `oops, you failed! :(`;
-let endingString2 = `Hooray!!! Be happy and positive`;
+let state = `title`; //it is title, simulation, ending with winning, or ending with losing.
 
 //setup
 //
@@ -56,47 +61,90 @@ function draw() {
   background(220,245,255);
 
 
-  //Non-user/medium purple circle's functions
-  move(); //this calls on function `move` that moves the purple circle.
-  displayNonUser(); //this calls on the function `displayNonUser` which draws the purple circle.
-
-  //User-control/yellow circle's functions.
-  arrowControl(); //this calls on the function `arrowControl` which draws and allows the user-control circle move with arrows.
-  moveUserControl(); //this calls on function `moveUserControl` that moves the yellow circle.
-  displayUserControl (); //this calls on the function `displayUserControl` which draws the yellow circle.
-  catchingPurpleCircle (); ////this calls on the function `catchingPurpleCircle` which stops the game if the bounce ball touches the purple circle/nonUser circle.
-
-  //Bounce balls functions.
-  catchingYellowCircle (); //this calls on the function `catchingYellowCircle` which stops the game if the bounce ball touches the yellow circle/userControl circle.
-  displayBounceBalls (); //this calls on function `displayBounceBalls` which draws the bounce balls and creates pink colors.
-  moveBounceballs ();  //this calls on function `moveBounceballs` that bounces the balls.
-
-
-  //changes the non-user circle's position randomly on the canvas.
-  let change = random(); //random number between 0 and 1.
-
-  //this number changes the direction of the circle 5% of the time.
-  if(change < 0.05) {
-    chaotic (); //this call on the function `chaotic` which makes the non-user circle to be chaotic and change its position randomly.
+  //if statement for the title, simulation, failed, and won.
+  if (state === `title`) {
+    //for the title: Don't get caught by negativity! Try to catch the purple circle with the arrow keys and avoid the pink circles. Press your mouse to get started.
+    title();
   }
-
-
+  else if (state === `simulation`) {
+    //for the simulation state
+    simulation();
+  }
+  else if (state === `failed`){
+    //ending 1: if the pink circles touches the yellow circles, the user loses.
+    failed();
+  }
+  else if (state === `won`) {
+    //ending 2: if the yellow circle touches the purple circle, the user wins.
+    won();
+  };
 };
 
-////////////////////////////////////////////////////////////////////////
+//title function right before the user presses the thier mouse.
+function title() {
+  push();
+  textSize(40);
+  fill(150,150,255);
+  textAlign(CENTER, CENTER);
+  text(`     Don't get caught by negativity!
+      Try to catch the purple circle with the
+      arrow keys and avoid the pink circles.
+      Press your mouse to get started.`, width/2, height/2);
+  pop();
+};
+
+//simulation function with all the other functions.
+function simulation() {
+  move();
+  displayNonUser();
+  arrowControl();
+  moveUserControl();
+  displayUserControl();
+  catchingPurpleCircle();
+  catchingYellowCircle();
+  displayBounceBalls();
+  moveBounceballs();
+};
+
+//ending 1 where the user loses because they touched the pink circles.
+function failed() {
+  push();
+  textSize(40);
+  fill(150,150,255);
+  textAlign(CENTER,CENTER);
+  text(`Oops, you failed :(`, width/2, height/2);
+  pop();
+};
+
+//ending 2 where the user wins the game and touches the purple circle.
+function won() {
+  push();
+  textSize(40);
+  fill(150,150,255);
+  textAlign(CENTER,CENTER);
+  text(`Hooray!!! you won :)`, width/2, height/2);
+  pop();
+};
+
+//Starts the game. This leads to the simulation when you press the mouse.
+function mousePressed() {
+  if (state === `title`) {
+    state = `simulation`;
+  }
+};
+
 /////////////////NonUser circle / medium purple circle//////////////////
-////////////////////////////////////////////////////////////////////////
 
   //This function moves the nonUser circle on x and y axis.
   function move () {
-    nonUser.x += nonUser.vx;
-    nonUser.y += nonUser.vy;
+    nonUser.x += nonUser.vx; //x-axis
+    nonUser.y += nonUser.vy; //y-axis
   };
 
 
   //This function display/draws the circle on the canvas and postion it on 500 x-axis and 350 y-axis.
   function displayNonUser () {
-    noStroke();
+    noStroke(); //no strokes
     fill(147,112,219) //medium purple color
     ellipse(nonUser.x, nonUser.y, nonUser.size);
   };
@@ -109,9 +157,8 @@ function draw() {
   };
 
 
-////////////////////////////////////////////////////////////////////////
 /////////////////UserControl circle / yellow circle/////////////////////
-////////////////////////////////////////////////////////////////////////
+
 
   //This function allows the user to control the yellow circle with the arrows key on the keyboard.
   function arrowControl () {
@@ -162,13 +209,13 @@ function draw() {
 
     //checks for catching yellow circle with if statement and also code line where the simulation stops if both circles touch each other.
     if (d < userControl.size/2 + nonUser.size/2) {
-      noLoop();
+      state = `won`;
     };
   };
 
-////////////////////////////////////////////////////////////////////////
+
 //////////////////////Bouncing balls/ pink circles /////////////////////
-////////////////////////////////////////////////////////////////////////
+
 
   //this function checks if the bounce balls touches the yellow circle or not. If they do then the game stops.
   function catchingYellowCircle () {
@@ -177,7 +224,7 @@ function draw() {
 
     //checks for catching bounce balls with if statement and also code line where the simulation stops if both circles touch each other.
     if (d < bounce.size/2 + userControl.size/2) {
-      noLoop();
+      state = `failed`;
     };
   };
 
@@ -193,3 +240,7 @@ function draw() {
     noStroke(); //nostrokes
     ellipse(bounce.x, bounce.y, 50, 50); //draws the yellow circle
   };
+
+
+
+  //Thank you so much and be happy!! :)))
