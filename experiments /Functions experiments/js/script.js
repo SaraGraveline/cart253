35bb9637cb47d2d6f23f4003b6dd042f39f656b3
SaraGraveline <graveline.sara@gmail.com>
2021-10-04 21:50:37 -0400

    learned about Automated movement

--- experiments /Functions experiments/js/script.js
+++ experiments /Functions experiments/js/script.js
@@ -7,18 +7,34 @@ This is document to experiments with functions and learn about them.
 
 "use strict";
 
-let bg = 0;
+let circle = {
+  x: 250,
+  y: 250,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speed: 2
+}
 
 
 function setup() {
   createCanvas(500, 500);
+
 };
 
 function draw() {
-  background(bg);
+  background(0);
 
-if (keyIsDown(65)) {
-  rectMode(CENTER);
-  rect(255, 255, 100, 100);
+  let change = random();
+  if(change < 0.05) {
+    circle.vx = random (-circle.speed, circle.speed);
+    circle.vy = random (-circle.speed, circle.speed);
   }
+
+
+  circle.x += circle.vx;
+  circle.y += circle.vy;
+
+  ellipse(circle.x, circle.y, circle.size);
+
 };
