e9d49fb72ae1e4867df3c525e2304b4a95de1cd3
SaraGraveline <graveline.sara@gmail.com>
2021-10-04 21:20:35 -0400

    learned about states

--- experiments /Functions experiments/js/script.js
+++ experiments /Functions experiments/js/script.js
@@ -7,19 +7,66 @@ This is document to experiments with functions and learn about them.
 
 "use strict";
 
+let circle = {
+  x: 0,
+  y: 250,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speed: 2
+};
+
+let state = `title`; //possible  states are titles, animation, ending
+
 
 function setup() {
   createCanvas(500, 500);
-}
+  circle.vx = circle.speed;
+  textSize(32);
+  textAlign(CENTER, CENTER);
+};
 
 function draw() {
-  background (127);
+  background(0);
 
-  textAlign(CENTER, CENTER);
-  textSize(64);
-  textStyle(BOLD);
+  if (state === `title`) {
+    title();
+  }
+  else if (state === `animation`) {
+    animation();
+  }
+  else if (state === `ending`) {
+    ending();
+  }
+};
 
-  fill(200, 100,60);
 
-  text(`Hello, world!`,250,250);
-}
+function title() {
+  fill(255);
+  text(`Life.`, width/2, height/2);
+};
+
+function animation () {
+  //animation
+  circle.x += circle.vx;
+  circle.y += circle.vy;
+
+  if (circle.x > width) {
+    state = `ending`;
+  }
+
+  ellipse(circle.x, circle.y, circle.size);
+};
+
+function ending () {
+  //ending
+  fill(127);
+  text(`It's all over.`, width/2, height/2);
+};
+
+
+function keyPressed () {
+  if (state ===  `title`) {
+    state = `animation`;
+  }
+};
