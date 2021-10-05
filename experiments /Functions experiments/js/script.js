066e131a639dd9e66ed44c58825fba956c8fb3ce
SaraGraveline <graveline.sara@gmail.com>
2021-10-04 20:31:51 -0400

    Learned about functions with parameters

--- experiments /Functions experiments/js/script.js
+++ experiments /Functions experiments/js/script.js
@@ -7,14 +7,6 @@ This is document to experiments with functions and learn about them.
 
 "use strict";
 
-let circle = {
-  x: 0,
-  y: 250,
-  size: 100,
-  vx: 1,
-  vy: 0
-};
-
 
 /**
 Description of setup
@@ -30,34 +22,18 @@ Description of draw()
 function draw() {
   background(0);
 
-  move();
-  wrap();
-  display();
+  parallels(100, 100, 5, 1, 100, 1);
+  parallels(50, 50, 10, 2, 20, 10);
+  parallels(200, 200, 15, 7, 3, 20);
+  parallels(312, 257, 20, .5, 300, 5);
 }
 
-function move(){
-    circle.x += circle.vx;
-    circle.y += circle.vy;
-  };
-
-function wrap() {
-  if (circle.x > width) {
-    reset();
+function parallels(x, y, numLines, lineWidth, lineHeight, lineSpacing) {
+  for (let i = 0; i < numLines; i++) {
+    noStroke ();
+    fill(255);
+    rectMode(CENTER);
+    rect(x, y, lineWidth, lineHeight);
+    x += lineSpacing;
   }
-};
-
-
-function display() {
-  fill (255, 0, 0);
-  ellipse(circle.x, circle.y, circle.size);
-};
-
-function reset() {
-  circle.x = 0;
-  circle.vx += 2;
-  circle.size += 5;
-}
-
-function mousePressed() {
-  reset();
 }
