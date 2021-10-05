8f75ee40271b0be1ef3b9b92ae2b5ce31a2cc36a
SaraGraveline <graveline.sara@gmail.com>
2021-10-04 20:13:24 -0400

    learned about function - video 1

--- experiments /Functions experiments/js/script.js
+++ experiments /Functions experiments/js/script.js
@@ -1,27 +1,26 @@
 /**
-Title of Project
-Author Name
+Function Experiments
+Sara Graveline
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+This is document to experiments with functions and learn about them.
 */
 
 "use strict";
 
-
-/**
-Description of preload
-*/
-function preload() {
-
-}
+let circle = {
+  x: 0,
+  y: 250,
+  size: 100,
+  vx: 1,
+  vy: 0
+};
 
 
 /**
 Description of setup
 */
 function setup() {
-
+  createCanvas(500, 500);
 }
 
 
@@ -29,5 +28,36 @@ function setup() {
 Description of draw()
 */
 function draw() {
+  background(0);
 
+  move();
+  wrap();
+  display();
+}
+
+function move(){
+    circle.x += circle.vx;
+    circle.y += circle.vy;
+  };
+
+function wrap() {
+  if (circle.x > width) {
+    reset();
+  }
+};
+
+
+function display() {
+  fill (255, 0, 0);
+  ellipse(circle.x, circle.y, circle.size);
+};
+
+function reset() {
+  circle.x = 0;
+  circle.vx += 2;
+  circle.size += 5;
+}
+
+function mousePressed() {
+  reset();
 }
