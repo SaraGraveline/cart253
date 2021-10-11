1de32d7691866f748c507fbf0a499915b765ae3e
SaraGraveline <graveline.sara@gmail.com>
2021-10-11 02:20:28 -0400

    P1- created the user control code

--- Project 1/js/script.js
+++ Project 1/js/script.js
@@ -1,33 +1,53 @@
 /**
-Title of Project
-Author Name
+Project 1
+Sara Graveline
 
 This is a template. You must fill in the title,
 author, and this description to match your project!
 */
 
+
 "use strict";
 
+//variable for the tail (user-control)
+let tail = [];
+let policeBadge;
 
-/**
-Description of preload
-*/
 function preload() {
-
+  policeBadge=loadImage('assets/images/police badge-02.png');
 }
 
 
-/**
-Description of setup
-*/
+// Setup function
+//
+// Draws the canvas.
 function setup() {
+  createCanvas(windowWidth, windowHeight);
+  noStroke();
+  noCursor();
+};
 
-}
 
-
-/**
-Description of draw()
-*/
+// Draw function
+//
+//Draws the mouse and mouse tail.
 function draw() {
+  background(0);
 
-}
+  // Creates the other circles at the end of the tail.
+  tail.push({x: mouseX, y: mouseY});
+
+  // This constrains the tail from getting to long.
+  if (tail.length > 10) {
+    tail.shift();
+  };
+
+  // This draws the tail.
+  for(let i = 0; i < tail.length; i++) {
+     let x = tail[i].x;
+     let y = tail[i].y;
+
+     // and larger closer to the mouse
+    image(policeBadge, x, y, 174 + i , 149 + i);
+  };
+};
