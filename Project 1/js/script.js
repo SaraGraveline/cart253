7869fe1f97c079ba962eed8db239c4cac162fd2c
SaraGraveline <graveline.sara@gmail.com>
2021-10-17 17:14:34 -0400

    P1- Add the theif elated code

--- Project 1/js/script.js
+++ Project 1/js/script.js
@@ -10,52 +10,171 @@ author, and this description to match your project!
 "use strict";
 
 //variable for the tail (user-control)
-let tail = [];
-let policeBadge;
-let diamond;
+let badge = [];
 
+//variable for the diamond
+let diamond = {
+  x: 450,
+  y: 150,
+  size: 50,
+};
+
+//variable for theif
+let theif = []; // the theif circles variables
+let posOfX = 850; //postion of the x where they all need to go
+let posOfY = 450; //postion of the y where they all need to go
+let theifAmount = 0; //the amount of the theif circle appares.
+let theifSpeedMultiplier = 2; //the spawn/speed of the theif circle
+let theifSizeMultiplier = 2; //size of the theif circle
+
+//variable for the images
+let policeBadge;
+let diamondImg;
+let thief;
+
+
+
+//preload
+//
+//displays all the images
 function preload() {
   policeBadge=loadImage('assets/images/police badge-02.png');
-  diamond=loadImage('assests/images/diamond-03.png');
+  diamondImg=loadImage('assets/images/diamond-03.png');
+  thiefImg=loadImage(`assets/images/thief-04.png`);
 };
 
 
-// Setup function
+//setup
 //
-// Draws the canvas.
+// Draws the canvas
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  noStroke();
-  noCursor();
+  noStroke();  //no stroke
+  noCursor(); //no cursor
 };
 
 
-// Draw function
-//
-//Draws the mouse and mouse tail.
-function draw() {
-  background(0);
 
-//---------------- user-control - Police badge ---------------
+// draw
+//
+//Draws everything else.
+function draw() {
+  background(0); //black bg
+
+
+//--------------- Displays the Diamond img ---------------
+  //image of the diamond
+  image(diamondImg, diamond.x, diamond.y, 850, 600);
+
+
+
+//--------------- user-control - Police badge ---------------
 
   // Creates the other circles at the end of the tail.
-  tail.push({x: mouseX, y: mouseY});
+  badge.push({x: mouseX, y: mouseY});
 
   // This constrains the tail from getting to long.
-  if (tail.length > 10) {
-    tail.shift();
+  if (badge.length > 10) {
+    badge.shift();
   };
 
-  // This draws the tail.
-  for(let i = 0; i < tail.length; i++) {
-     let x = tail[i].x;
-     let y = tail[i].y;
+  // This draws the tail of more police badges.
+  for(let i = 0; i < badge.length; i++) {
+     let x = badge[i].x;
+     let y = badge[i].y;
 
-     // and larger closer to the mouse
+  // displays the policeBadge image.
     image(policeBadge, x, y, 174 + i , 142 + i);
   };
 
+//---------------Thiefs - Non User-controled-----------
+
+  //the amount, spawn and new theif circles code
+  theifAmount += 1; //the amount of theif plus 1
+  let spawnInterval = int(200 / theifSpeedMultiplier)
+  //new theif circles if statement and their spawn
+  if (theifAmount % spawnInterval == 0){
+    let newTheif = new balloon(); //see function class below (by TANMAYA15 - editer p5js.org)
+    theif.push(newTheif);
+  };
 
 
 
+//-------------------------------------------Theif circles----------------------------------------
+for (var i = 0; i < theif.length; i++){
+  theif[i].draw(); //see the function of draw below
+  theif[i].new(); // see the new function below
+  if (theif[i].outSide()){ //see the outSide function below
+        theif.splice(i,1);
+    };
+};
+
+//speed and the size of theif circles
+theifSpeedMultiplier += 0.002;
+  if (theifSizeMultiplier < 8){
+    theifSizeMultiplier += 0.002;
+  };
+};
+
+
+
+// the class function of the theif ballon (by TANMAYA15 - editer p5js.org)
+class balloon{
+	constructor(){
+		this.side = int(random(4));
+		switch (this.side)
+		{
+      //Case 0 has a x with 0 value and y with random height
+			case 0:
+				this.x = 0;
+				this.y = int(random(height));
+				break;
+      //Case 1 has a x with random width and y with 0 value
+			case 1:
+				this.x = int(random(width));
+				this.y = 0;
+				break;
+      //Case 2 has a x with width value and y with random height
+			case 2:
+				this.x = width;
+				this.y = int(random(height));
+				break;
+      //Case 3 has a x random width and y with height value
+			case 3:
+				this.x = int(random(width));
+				this.y = height;
+				break;
+		};
+    //the theif x and theif y is equall to the position of x and y (the variable listed above)
+		this.theifX = posOfX;
+		this.theifY = posOfY;
+    //this theif dir create a vector with theif x and theif y - the this x and y which is listed in the class balloon.
+		this.theifDir = createVector(this.theifX - this.x, this.theifY - this.y);
+		this.theifDir.normalize();
+    //this x and y speed is equal to the theif's dir with times the theif speed Multiplier
+		this.xSpeed = this.theifDir.x*theifSpeedMultiplier;
+		this.ySpeed = this.theifDir.y*theifSpeedMultiplier;
+		this.r = 12*theifSizeMultiplier;
+	};
+
+
+//This is the function that displays the theif circles
+	draw(){
+		push();
+		noStroke(); //no strokes
+		fill(255, 0, 0); //red fill
+		ellipse(this.x, this.y, this.r); //draws the ellipse
+		pop();
+	};
+
+  // this is the function that updates the this.x and y in the class ballon to equal to its x and y speed.
+	 new(){
+		this.x += this.xSpeed;
+		this.y += this.ySpeed;
+	};
+
+//this function returns the circles with the this.x or this y
+	outSide(){
+		return(this.x > width+10 || this.x < -10 || this.y > height+10 || this.y < -10);
+	};
 };
