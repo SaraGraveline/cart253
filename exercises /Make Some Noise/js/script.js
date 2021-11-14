7277106374ac555fd17c882792e56c38206407d5
SaraGraveline <graveline.sara@gmail.com>
2021-11-13 20:12:54 -0500

    E6 - added the descriptions of this exercise like (what is this project about)

--- exercises /Make Some Noise/js/script.js
+++ exercises /Make Some Noise/js/script.js
@@ -1,15 +1,16 @@
 /**
-Title of Project
-Author Name
+E6 - Make Some Noise
+Sara Graveline
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
-*/
+This exercise is to explore sounds in p5.js. Through this excercise, I created a sound cursor which I use in my final project.
+Becuase my final project theme is christmas, I used the song silent night. As the song starts, the circles starts to bounce with the rhythm of the song.
+To create more christmas environment, I used random function on the stroke of these circles.
+This cursor will be used on a landing page and the ending page of the game where the user will have to click on different buttons.
 
-"use strict";
-
-
+Credits: Sound visualization by Lllucas
 /**
+
+
 Description of preload
 */
 function preload() {
