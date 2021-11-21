5076a8d273b42228dd8d8a7769ec736a97dc120b
SaraGraveline <graveline.sara@gmail.com>
2021-11-21 14:31:44 -0500

    Experiment: learned about overriding methods

--- experiments /Inheritance experiments /js/Car.js
+++ experiments /Inheritance experiments /js/Car.js
@@ -5,9 +5,32 @@ class Car extends Vehicle {
     this.width = 50;
     this.height = 20;
     this.vx = 5;
+    this.drunkeness= 0.2;
   }
 
-  
+  move() {
+    this.veer();
+    super.move();
+  }
+
+  veer() {
+    let r = random();
+    if (r < this.drunkeness) {
+      this.vy = random(-5,5);
+    }
+  }
+
+  wrap() {
+    super.wrap();
+
+    if (this.y > height) {
+      this.y -= height;
+    }
+    else if (this.y < 0) {
+      this.y += height;
+    }
+  }
+
   display() {
     push();
     rectMode(CENTER);
