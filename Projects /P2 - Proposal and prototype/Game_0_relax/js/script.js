0e5a3dd4a6f3fcd41fc95cb595907cb7332af7b2
SaraGraveline <graveline.sara@gmail.com>
2021-12-07 12:08:11 -0500

    Project 2: updated and added comments on the game 0- relax

--- Projects /P2 - Proposal and prototype/Game_0_relax/js/script.js
+++ Projects /P2 - Proposal and prototype/Game_0_relax/js/script.js
@@ -1,67 +1,75 @@
 /**
-E6 - Make Some Noise
+Project 2: Game 0 - Relax
 Sara Graveline
 
-This exercise is to explore sounds in p5.js. Through this exercise, I created a sound cursor which I use in my final project.
-Because my final project theme is christmas, I used the song silent night. As the song starts, the circles starts to bounce with the rhythm of the song.
-To create more christmas environment, I used random function on the stroke of these circles.
-This cursor will be used on a landing page and the ending page of the game where the user will have to click on different buttons.
+This game is the beginning of the final 3 games that the user will be playing to help jungkook throw a christmas party.
+This relax game is the starting point because with everyhting that we went through with covid, being online, and not being able to live a normal life is very hard.
+So this game lets the user take a few minutes to just relax and breath without any concerns or worry. And whenever the user want to go on and start playing the game, she/he can click the link whcih leads to the game 1 - collecting gifts.
 
-Credits: Sound visualization by Lllucas
-**/
+The main goal of game 0 is to play with sounds. So I created a sound cursor, inspried by Lllucas's sound visualization.
+When the user presses any key on their keyword, silent night starts playing and the circles of the cursor starts to bounce with the rhythm of the song.
+
+Credits: Sound visualization by Lllucas = https://editor.p5js.org/Lllucas/sketches/dDxXHIbtD
+        CreateA = https://p5js.org/reference/#/p5/createA
+*/
 
 let silentNight; //the song
 let bounce = []; //rhythm to the song
 let base; //for the get level p5.js
 
-
 /**
 This function loads the silent night mp3 song from the sound assets folder.
 */
 function preload() {
- silentNight = loadSound("assets/sounds/SILENT-NIGHT.mp3");
-};
+  silentNight = loadSound("assets/sounds/SILENT-NIGHT.mp3");
+}
 
 /**
 This function setup the canvas to windowWidth and windowHeight.
-It also plays the song as the page load and plus the base varible.
+With a createA I linked my games togther. SO this CreateA links is for game 1-collect gifts.
 */
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  let a = createA('https://saragraveline.github.io/cart253/Projects%20/P2%20-%20Proposal%20and%20prototype/Game_1_Collect_gifts/', 'Start Game');
-  a.position(width/50, height/8);
+  let a = createA(
+    "https://saragraveline.github.io/cart253/Projects%20/P2%20-%20Proposal%20and%20prototype/Game_1_Collect_gifts/",
+    "Start Game"
+  );
+  a.position(width / 50, height / 8);
 
   base = new p5.Amplitude(); //from p5.js
+}
 
 
-};
-
+//This makes the song play when any key is pressed.
 function keyPressed() {
   silentNight.play();
-};
-
+}
 
 /**
 This function draws random strokes colors for the circles, bounces all the circles, and the movement of the circle with mouse.
+It also displays the text of instruction of what to do with displayText function.
 */
 function draw() {
-  background(0); //black color background
+  background(0);
 
-  displayText(`Finally, it's christmas!! Time to relax and have fun.
+  //the instruction of what to do and what this is. This calls upon display text function.
+  displayText(`Finally, it's Christmas!! Time to relax and have fun.
 Press any key one time to relax and
-whenever ready press this button to start the game.`);
+whenever ready press the link below to start the game.`);
 
   let vol = base.getLevel();
   bounce.push(vol);
 
-  colorMode(HSB); //sets the color mode to HSB
-  stroke(random(10, 240), random(10, 240), random(10, 240)); //random color strokes for all of the circles.
+  //sets the color mode to HSB and random color are sit on all of the strokes of circles.
+  colorMode(HSB);
+  stroke(random(10, 240), random(10, 240), random(10, 240));
 
   noFill(); //no fill
   noCursor(); // no cursor on the canvas
 
-  translate(mouseX, mouseY); //able to move the circle around
+  //able to move the circle around
+  translate(mouseX, mouseY);
 
   //controls the rhythm of the circles.
   for (i = 0; i < 180; i++) {
@@ -69,28 +77,23 @@ whenever ready press this button to start the game.`);
     let x = r * cos(i);
     let y = r * sin(i);
 
-    ellipse(x, y, 10, 10); //draws the circle
-  };
+    //draws the circle
+    ellipse(x, y, 10, 10);
+  }
 
   // makes the circle move back and fourth like they are bouncing.
   if (bounce.length > 180) {
     bounce.splice(0, 1);
-  };
+  }
+}
 
-};
-
+//function for the instruction text
 function displayText(string) {
   push();
   textAlign(LEFT);
   textSize(20);
   noStroke();
-  fill(60);
-  text(string, width/50, height/20);
+  fill(80);
+  text(string, width / 50, height / 20); //displays it on the left top.
   pop();
 };
-
-/*
-function startGame() {
-	open('https://saragraveline.github.io/cart253/Projects%20/P2%20-%20Proposal%20and%20prototype/Collecting%20gifts/');
-}
-*/
