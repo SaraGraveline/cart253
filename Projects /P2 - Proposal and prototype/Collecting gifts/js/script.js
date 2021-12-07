6283c976724eea046d95c44dbbeb2f0fe72375c3
SaraGraveline <graveline.sara@gmail.com>
2021-12-06 23:33:11 -0500

    Project 2: deleted the list of things that were needed to be done

--- Projects /P2 - Proposal and prototype/Collecting gifts/js/script.js
+++ Projects /P2 - Proposal and prototype/Collecting gifts/js/script.js
@@ -5,12 +5,6 @@ Sara Graveline
 This is the third game of my final game. I am coding the snake game where the player will have to collect the presents before the time runs out.
 There will be about 50 presents and after the player successfully collects all, she/he can go back to continue the main game.
 
- --- need to add ---
-  1. timer
-  2. begin screen
-  3. ending screen
-  4. gif of a present
-  5. marge it with other games
 */
 
 "use strict";
