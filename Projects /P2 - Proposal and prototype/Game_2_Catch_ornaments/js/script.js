248543ede1d658d40e3d3b25fcdc21f69efb0c93
SaraGraveline <graveline.sara@gmail.com>
2021-12-08 00:43:02 -0500

    Project 2: game 2 - cleaned up the js file and added the a better bg to simulation

--- Projects /P2 - Proposal and prototype/Game_2_Catch_ornaments/js/script.js
+++ Projects /P2 - Proposal and prototype/Game_2_Catch_ornaments/js/script.js
@@ -2,7 +2,7 @@
 Project 2 - Catch the ornaments
 Sara Graveline
 
-The player has to help Jungkook with catching all the ornaments. Don't let them touch the ground becuase it will break if it does. And after catching 25 of them, you can go back to the main (matching game) and complete that game.
+The ornaments has to help Jungkook with catching all the ornaments. Don't let them touch the ground becuase it will break if it does. And after catching 25 of them, you can go back to the main (matching game) and complete that game.
 
 credits: mouse trails -- by mrbombmusic
 ****************************************************/
@@ -10,18 +10,19 @@ credits: mouse trails -- by mrbombmusic
 "use strict";
 
 //Variables for images.
-let bgStartScreen; //start screen image variable
-let bgPlayMode; //middle/game screen image variable
-let bgEndScreen; //end screen image variable
-let bgWonScreen;
-let ornaments; //
-let badge; //
+let titleImage; //start screen image variable
+let simulationImage; //middle/game screen image variable
+let deadImage; //end screen image variable
+let wonImage;
+
+let ornamentsImage; //
+let hand; //
 
 //Variable for the screen, the function is to switch between start, middle and end screens.
-let state = 0;
+let state = `title`;
 
 //Variable for ornaments/non-user controled.
-let player = {
+let ornaments = {
   x: 0,
   y: 0,
   size: 50,
@@ -29,8 +30,8 @@ let player = {
   score: 0,
 };
 
-//Variable for the player/User-controlled mouse tail .
-let tail = [];
+//Variable for the ornaments/User-controlled mouse tail .
+let mouse = [];
 let a = 0;
 
 //Variable for the font, Astron by Mason Mulcahy.
@@ -38,55 +39,49 @@ let astron;
 
 //Loads the images and font for each element that requires an image or font.
 function preload() {
-  //start screen background image
-  bgStartScreen = loadImage(`assets/images/startpage_game2.jpg`);
-  //middle/during game background image
-  bgPlayMode = loadImage(`assets/images/dark-image.jpg`);
-  //end/game over background image
-  bgEndScreen = loadImage(`assets/images/deadstate_image .jpg`);
+  titleImage = loadImage(`assets/images/startpage_game2.jpg`);  //start screen background image
+  simulationImage = loadImage(`assets/images/simulation _gradient_Image_game2.jpg`); //middle/during game background image
+  deadImage = loadImage(`assets/images/deadstate_image .jpg`); //end/game over background image
+  wonImage = loadImage(`assets/images/wonstate_image.jpg`);//won/game over background image
 
-  bgWonScreen = loadImage(`assets/images/wonstate_image.jpg`);
 
-  //tornaments/non-user-controlled image
-  ornaments = loadImage(`assets/images/ornaments-ball.png`);
-  //player/user-controlled image
-  badge = loadImage(`assets/images/hand.png`);
+  ornamentsImage = loadImage(`assets/images/ornaments-ball.png`); //tornaments/non-user-controlled image
+  hand = loadImage(`assets/images/hand.png`);   //ornaments/user-controlled image
 
-  //Astron font for all the text.
-  astron = loadFont(`assets/fonts/Astron.ttf`);
+  astron = loadFont(`assets/fonts/Astron.ttf`);   //Astron font for all the text.
 }
 
-//This sets the basic things that are needed for this game like canvas, player's x and size and also noCursor throughout the whole program.
+//This sets the basic things that are needed for this game like canvas, ornaments's x and size and also noCursor throughout the whole program.
 function setup() {
   createCanvas(windowWidth, windowHeight); //creates the canvas
-  player.x = random(width); //randomizes where the circle comes from on the x axis.
-  player.size = random(40, 80); //randomizes the size of the circle.
   noCursor(); //no cursor at all
+  ornaments.x = random(width); //randomizes where the circle comes from on the x axis.
+  ornaments.size = random(40, 80); //randomizes the size of the circle.
 }
 
 //This function displays and creates the whole game.
 function draw() {
   //if statement for the state variable. This changes the screens from start, middle or to end.
-  if (state == 0) {
+  if (state === `title`) {
     title();
-  } else if (state == 1) {
-    playMode();
-  } else if (state == 2) {
-    dead();
-  } else if (state == 3) {
+  }else if (state ===`simulation`) {
+    simulation();
+  } else if (state === `won`) {
     won();
+  } else if (state === `dead`) {
+    dead();
   }
-}
+};
 
 //This function is for the start screen/ intro screen.
 function title() {
-  background(bgStartScreen); //background image
-  reset(); //reset function when the player lose the game they come back to this.
+  background(titleImage); //background image
+  reset(); //reset function when the ornaments lose the game they come back to this.
 }
 
 //This function runs and displays the whole games
-function playMode() {
-  background(bgPlayMode); //background image
+function simulation() {
+  background(simulationImage); //background image
   noStroke();
 
   //text and placement for the score numbers on the top right corner
@@ -94,64 +89,67 @@ function playMode() {
   textFont(`astron`);
   fill(255);
   text(`Your score =`, 20, 63); //text with the x and y position
-  text(player.score, 213, 62); //score number with the x and y position
+  text(ornaments.score, 213, 62); //score number with the x and y position
 
   //displays and shows that image of the ornaments.
-  image(ornaments, player.x, player.y, player.size, player.size);
+  image(ornamentsImage, ornaments.x, ornaments.y, ornaments.size, ornaments.size);
 
   //makes the ornaments move on the canvas
-  player.y += player.speed;
+  ornaments.y += ornaments.speed;
+
+  //constraining it in the canvas
+  ornaments.x = constrain(ornaments.x, 0, width -ornaments.size);
 
   //if statement of when the layer hits the bottom, the end screen appears (game over).
-  if (player.y > height) {
-    state = 2; //end screen
+  if (ornaments.y > height) {
+    state = `dead`; //end screen
   }
 
   //if statement for when the circle hits the bottom, a new random size, speed, and on different x axis, a new ornaments appears.
-  if (player.y > height) {
-    player.x = random(width); //random x-axis
-    player.speed = random(3, 7); // random speed
-    player.size = random(40, 70); // random size
-    player.y = 0; //the ornaments always appears on the 0 value of the y-axis.
+  if (ornaments.y > height) {
+    ornaments.x = random(width); //random x-axis
+    ornaments.speed = random(3, 7); // random speed
+    ornaments.size = random(40, 70); // random size
+    ornaments.y = 0; //the ornaments always appears on the 0 value of the y-axis.
   }
 
-  //if statement for when the player badge touches a ornaments.
+  //if statement for when the ornaments hand touches a ornaments.
   if (
-    mouseX > player.x - 40 &&
-    mouseX < player.x + 40 &&
-    mouseY > player.y - 40 &&
-    mouseY < player.y + 40
+    mouseX > ornaments.x - 40 &&
+    mouseX < ornaments.x + 40 &&
+    mouseY > ornaments.y - 40 &&
+    mouseY < ornaments.y + 40
   ) {
-    player.y = 0;
-    player.x = random(width);
-    player.score = player.score + 1; // the player scores when the player badge catches the ornaments.
-    player.speed = random(3, 12);
-    player.size = random(40, 80);
+    ornaments.y = 0;
+    ornaments.x = random(width);
+    ornaments.score = ornaments.score + 1; // the ornaments scores when the ornaments hand catches the ornaments.
+    ornaments.speed = random(3, 12);
+    ornaments.size = random(40, 80);
   }
 
-  //this is for the player badge/user-controlled.
-  tail.push([mouseX, mouseY]);
-  for (let i = 0; i < tail.length; i++) {
+  //this is for the ornaments hand/user-controlled.
+  mouse.push([mouseX, mouseY]);
+  for (let i = 0; i < mouse.length; i++) {
     noStroke();
-    //displays the image, size of the player badge
-    image(badge, tail[i][0], tail[i][1], 110, 100, a);
+    //displays the image, size of the ornaments hand
+    image(hand, mouse[i][0], mouse[i][1], 110, 100, a);
     if (a > 100) {
-      tail.shift();
+      mouse.shift();
       a = 10;
     }
     a += 8;
   }
 
-  //if the player has 20 gift/score, the game is won with a gamewon music and no start song.
-  if (player.score === 3) {
-    state = 3;
+  //if the ornaments has 20 gift/score, the game is won with a gamewon music and no start song.
+  if (ornaments.score === 30) {
+    state = `won`;
   }
 }
 
 //This function displays and runs the end screen of this game
 function dead() {
-  background(bgEndScreen); //background image
-  displayText(`Score = ` + player.score);
+  background(deadImage); //background image
+  displayText(`Score = ` + ornaments.score);
 }
 
 //display text function for dead state function.
@@ -166,13 +164,13 @@ function displayText(string) {
 }
 
 function won() {
-  background(bgWonScreen);
+  background(wonImage);
   push();
   textAlign(CENTER, CENTER);
   textSize(30);
   noStroke();
   fill(255);
-  text(`Score = ` + player.score, width / 1.23, height / 1.46);
+  text(`Score = ` + ornaments.score, width / 1.23, height / 1.46);
   cursor();
   pop();
 
@@ -185,21 +183,21 @@ function won() {
 
 //this function is for moving from one screen to the next whenever the mouse is pressed.
 function mousePressed() {
-  //if the player is on the start screen and she/he presses the mouse, they will go to the play mode screen(simulation state).
-  if (state == 0) {
-    state = 1;
-    //if the player is on the end screen - game over, then she/he will go the state screen if the mouse is pressed.
-  } else if (state == 2) {
-    state = 0;
+  //if the ornaments is on the start screen and she/he presses the mouse, they will go to the play mode screen(simulation state).
+  if (state === `title`) {
+    state = `simulation`;
+    //if the ornaments is on the end screen - game over, then she/he will go the state screen if the mouse is pressed.
+  } else if (state === `dead`) {
+    state = `title`;
   }
 }
 
 //this function resets the game.
 function reset() {
-  player.state = 0;
-  player.speed = 1;
-  player.y = -2;
-  player.score = 0;
+  ornaments.state = 0;
+  ornaments.speed = 1;
+  ornaments.y = -2;
+  ornaments.score = 0;
 }
 
 //Thank you and have fun playing this game! :):)
